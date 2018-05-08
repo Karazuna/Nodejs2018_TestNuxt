8d8d751f54ce44ce63ee2819b8cbff7f6885c49e
Karazuna <kchy12345@gmail.com>
2018-05-08 12:55:53 +0900

    2018-05-08, NuxtJS - starterTemplate & practice

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+<section class="section">
+  <div class="block">
+      About page
+  </div>
+  <nuxt-link to="/about"></nuxt-link>
+</section>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+<section class="section">
+  <div class="block">
+    <a href="#" class="button is-primary">Test</a>
+    <p>클라우드 서비스용 포트폴리오</p>
+    <img src="~static/DY4xAVMXUAE1TpA.jpg" alt="">
+    <br>
+    <img src="~assets/DY4xAVMXUAE1TpA.jpg" alt="">
+  </div>
+  <nuxt-link to="/about">linkTest</nuxt-link>
+</section>
+
+</template>
